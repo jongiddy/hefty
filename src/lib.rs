0974befa8e45cbe9f1d7c6a32cbbe8451f725137
Jonathan Giddy <jongiddy@gmail.com>
2023-10-16 18:53:16 +0100

    Add when for functions on non-ref byte and char

--- src/lib.rs
+++ src/lib.rs
@@ -188,6 +188,40 @@ struct ByteWhenParser<F>(F);
 
 impl<F> Extract for ByteWhenParser<F>
 where
+    F: Fn(u8) -> bool,
+{
+    type State = ();
+
+    fn extract(
+        &self,
+        input: &mut Bytes,
+        _state: Option<Self::State>,
+    ) -> ParseResult<Self::State, Output> {
+        match input.first() {
+            Some(&b) if (self.0)(b) => ParseResult::Match(Output {
+                chunks: OutputChunks::One(input.split_to(1)),
+            }),
+            Some(_) => ParseResult::NoMatch,
+            None => ParseResult::Partial(()),
+        }
+    }
+}
+
+impl<F> ParseWhen<u8, F> for u8
+where
+    F: Fn(u8) -> bool,
+{
+    type Parser = ByteWhenParser<F>;
+
+    fn when(f: F) -> Self::Parser {
+        ByteWhenParser(f)
+    }
+}
+
+struct ByteWhenRefParser<F>(F);
+
+impl<F> Extract for ByteWhenRefParser<F>
+where
     F: Fn(&u8) -> bool,
 {
     type State = ();
@@ -211,10 +245,10 @@ impl<F> ParseWhen<&u8, F> for u8
 where
     F: Fn(&u8) -> bool,
 {
-    type Parser = ByteWhenParser<F>;
+    type Parser = ByteWhenRefParser<F>;
 
     fn when(f: F) -> Self::Parser {
-        ByteWhenParser(f)
+        ByteWhenRefParser(f)
     }
 }
 
@@ -302,6 +336,69 @@ struct CharWhenParser<F>(F);
 
 impl<F> Extract for CharWhenParser<F>
 where
+    F: Fn(char) -> bool,
+{
+    type State = (usize, Output);
+
+    fn extract(
+        &self,
+        input: &mut Bytes,
+        state: Option<Self::State>,
+    ) -> ParseResult<Self::State, Output> {
+        let (mut required, mut output) = state.unwrap_or((0, Output::default()));
+        if required == 0 {
+            match input.first() {
+                Some(&b) => {
+                    required = utf8_char_width(b);
+                    if required == 0 {
+                        return ParseResult::NoMatch;
+                    }
+                }
+                None => {
+                    return ParseResult::Partial((required, output));
+                }
+            }
+        }
+        if input.len() < required {
+            required -= input.len();
+            output.push(input.split_off(0));
+            ParseResult::Partial((required, output))
+        } else {
+            output.push(input.split_to(required));
+            let mut bytes = [0; 4];
+            let len = output.copy_to_slice(&mut bytes);
+            match std::str::from_utf8(&bytes[..len]) {
+                Ok(s) => {
+                    let Some(c) = s.chars().next() else {
+                        return ParseResult::NoMatch;
+                    };
+                    if (self.0)(c) {
+                        ParseResult::Match(output)
+                    } else {
+                        ParseResult::NoMatch
+                    }
+                }
+                Err(_) => ParseResult::NoMatch,
+            }
+        }
+    }
+}
+
+impl<F> ParseWhen<char, F> for char
+where
+    F: Fn(char) -> bool,
+{
+    type Parser = CharWhenParser<F>;
+
+    fn when(f: F) -> Self::Parser {
+        CharWhenParser(f)
+    }
+}
+
+struct CharWhenRefParser<F>(F);
+
+impl<F> Extract for CharWhenRefParser<F>
+where
     F: Fn(&char) -> bool,
 {
     type State = (usize, Output);
@@ -354,10 +451,10 @@ impl<F> ParseWhen<&char, F> for char
 where
     F: Fn(&char) -> bool,
 {
-    type Parser = CharWhenParser<F>;
+    type Parser = CharWhenRefParser<F>;
 
     fn when(f: F) -> Self::Parser {
-        CharWhenParser(f)
+        CharWhenRefParser(f)
     }
 }
 
@@ -404,6 +501,21 @@ mod tests {
         let buffer = Bytes::from_static("A4".as_bytes());
         let mut input = buffer.clone();
         assert_eq!(input.len(), 2);
+        let res = u8::when(|b: u8| u8::is_ascii_alphabetic(&b)).extract(&mut input, None);
+        assert_matches!(res, ParseResult::Match(output) if output.to_string() == "A");
+        let res = u8::when(|b: u8| u8::is_ascii_alphabetic(&b)).extract(&mut input.clone(), None);
+        assert_matches!(res, ParseResult::NoMatch);
+        let res = u8::when(|b: u8| u8::is_ascii_digit(&b)).extract(&mut input, None);
+        assert_matches!(res, ParseResult::Match(output) if output.to_string() == "4");
+        let res = u8::when(|b: u8| u8::is_ascii_digit(&b)).extract(&mut input, None);
+        assert_matches!(res, ParseResult::Partial(_state));
+    }
+
+    #[test]
+    fn test_byte_when_ref() {
+        let buffer = Bytes::from_static("A4".as_bytes());
+        let mut input = buffer.clone();
+        assert_eq!(input.len(), 2);
         let res = u8::when(u8::is_ascii_alphabetic).extract(&mut input, None);
         assert_matches!(res, ParseResult::Match(output) if output.to_string() == "A");
         let res = u8::when(u8::is_ascii_alphabetic).extract(&mut input.clone(), None);
@@ -446,8 +558,19 @@ mod tests {
         assert_matches!(res, ParseResult::Partial(_state));
 
         let mut input = buffer.clone();
+        let res = char::when(char::is_alphabetic).extract(&mut input.clone(), None);
+        assert_matches!(res, ParseResult::NoMatch);
+        let res = char::when(|c: char| !c.is_alphabetic()).extract(&mut input, None);
+        assert_matches!(res, ParseResult::Match(output) if output.to_string() == "€");
+        let res = char::when(|c| char::is_digit(c, 10)).extract(&mut input, None);
+        assert_matches!(res, ParseResult::Match(output) if output.to_string() == "4");
+        let res = char::when(char::is_alphabetic).extract(&mut input, None);
+        assert_matches!(res, ParseResult::Partial(_state));
+
+        let mut input = buffer.clone();
         let res = char::when(char::is_ascii_alphabetic).extract(&mut input.clone(), None);
         assert_matches!(res, ParseResult::NoMatch);
+        // closure with ref arg does not get inferred for<'a> lifetime if placed in when arg
         let not_alphabetic = |c: &char| -> bool { !c.is_ascii_alphabetic() };
         let res = char::when(not_alphabetic).extract(&mut input, None);
         assert_matches!(res, ParseResult::Match(output) if output.to_string() == "€");
